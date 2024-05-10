6e368e2bf1d49dda7b5b2a844854494f94614752
AlyanTariq90 <156662371+AlyanTariq90@users.noreply.github.com>
2024-05-11 01:55:49 +0500

    Add files via upload

---
+++ haker rank/rank.js
@@ -0,0 +1,23 @@
+// function add(ar) {
+//   let sum = 0;
+//   for (i = 0; i < ar.length; i++) {
+//     sum += ar[i];
+//   }
+//   return sum;
+// }
+// // var arry = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+// console.log(add([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]));
+
+
+
+
+// function add(a){
+//         let sum = 0
+//         for (var i=0; i<a.length; i++){
+//             sum = sum + a[i]
+//         } 
+//         return sum
+//     }
+//     console.log(add([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
+
+
